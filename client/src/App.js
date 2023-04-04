b496e918453b3aeeec32a1ecd2c5b34a0eb974da
yuvrajcell-99 <yyyuvraj11@gmail.com>
2023-04-04 23:39:40 +0530

    routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,74 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './App.css';
 
+/* ===============import all components================ */
+
+import PageNotFound from './components/PageNotFound';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import UserName from './components/UserName';
+
+/* ===============Import End ======================= */
+
+
+
+/*===================All routes======================= */
+
+
+
+const router= createBrowserRouter([
+  {
+    path : '/',
+    element:<UserName>UserName</UserName>
+  
+  },
+  {
+    path : '/register',
+    element:<Register>Register Route</Register>
+  
+  },
+  {
+    path : '/profile',
+    element:<Profile>Profile</Profile>
+  
+  },
+  
+  {
+    path : '/password',
+    element:<Password>Password</Password>
+  
+  },
+  {
+    path : '/recovery',
+    element:<Recovery>Recovery </Recovery>
+  
+  },
+  {
+    path : '/reset',
+    element:<Reset>Reset</Reset>
+  
+  },
+  {
+    path : '*',
+    element:<PageNotFound>Page Not found </PageNotFound>
+  
+  },
+
+
+  /* ==================All routes ends======================  */
+
+])
 function App() {
   return (
-    <div className="App">
-      Hello React app 
-    </div>
+    <main>
+   
+     <RouterProvider router={router}></RouterProvider>
+
+  </main>
   );
 }
 
